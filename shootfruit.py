85fe65606a39c2f94fcbb8880e896f1e77de36c5
aslicode <asliladaku@gmail.com>
2020-09-20 15:57:08 +0530

    update with scores and game over screen

--- shootfruit.py
+++ shootfruit.py
@@ -1,10 +1,18 @@
 import pgzrun
+import pygame
 from random import randint
 apple=Actor("apple")
-
+game_over=False
+score=0
 def draw():
     screen.clear()
+    screen.fill("light green")
+    screen.draw.text("Score "+str(score),color=("violet"),topleft=(10,10),fontsize=60)
     apple.draw()
+    if game_over:
+        screen.fill("black")
+        screen.draw.text(" GAME OVER ",color=("DARK RED"),topleft=(250,10),fontsize=60)
+        screen.draw.text("YOUR FINAL SCORE WAS : "+str(score),color=("white"),topleft=(210,90),fontsize=40)
 
 def place_apple():
     apple.x=randint(10,800)
@@ -12,16 +20,14 @@ def place_apple():
 
 
 def on_mouse_down(pos):
-    score=0
+    global score
+    global game_over
     if apple.collidepoint(pos):
-        print("GOOD SHOT")
         place_apple()
         score=score+1
     else:
+        game_over=True
         print("You missed")
-        quit()
     
 place_apple()
 pgzrun.go()
-#This is an branch to the master
-#this is the branch with 2nd edit
